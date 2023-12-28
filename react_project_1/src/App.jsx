502f4e398005e4b10cb99d0b693ad7008151de55
Sakil M <sakilmahmudmolla@gmail.com>
2023-12-28 17:03:51 +0530

    code updated

--- react_project_1/src/App.jsx
+++ react_project_1/src/App.jsx
@@ -1,9 +1,27 @@
+import "./assets/styles/card.css"
 
 function App() {
+  
+  const cardDesign = {
+    backgroundColor :  "#efefef",
+    padding: "10px",
+    width: "10%"
+  }
 
   return (
     <>
-    <h1>Hello World!</h1>
+    <div>
+      <div style={cardDesign}>
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+      <div className="card">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
     </>
   )
 }
